0a7b611384cfd4af544e5b73311fee6b2abd5cbf
Minh Nguyen <hoaiminh4321@gmail.com>
2021-11-11 07:46:35 +0700

    update

--- asset/js/db/Q&A.js
+++ asset/js/db/Q&A.js
@@ -18,6 +18,21 @@
         "Id": "qa4",
         "Question": "Trường có mấy cơ sở?",
         "Answer": "Hiện tại Phổ thông Cao đẳng đang triển khai tại 09 cơ sở trên toàn quốc. Các cơ sở đào tạo tọa lạc tại các thành phố lớn, trung tâm của Việt Nam như: Hà Nội, Đà Nẵng, HCM, Tây Nguyên và Cần Thơ,…\n– Cơ sở Hà Nội: Đường Trịnh Văn Bô, phường Phương Canh, quận Nam Từ Liêm, Hà Nội.\n– Cơ sở Đà Nẵng: Số 137 Nguyễn Thị Thập, phường Hoà Minh, quận Liên Chiểu, TP. Đà Nẵng.\n- Cơ sở Tây Nguyên: Số 27, đường Nguyễn Tất Thành, Tân Lợi, TP. Buôn Mê Thuột, Đắk Lắk.\n- Cơ sở Hồ Chí Minh: Toà nhà Innovation, lô 24, Công viên phần mềm Quang Trung, Quận 12, Hồ Chí Minh.\n- Cơ sở Cần Thơ: Số 288, Nguyễn Văn Linh, phường An Khánh, quận Ninh Kiều, TP. Cần Thơ.\n– Cơ sở Đồng Nai: Số 120/68/39 Lê Thoa, Phường Tân Tiến, TP. Biên Hòa, Đồng Nai.\n– Cơ sở Huế:  Số 16 Lâm Hoằng, Phường Vỹ Dạ, TP. Huế.\n– Cơ sở Hải Phòng:  118A Cát Bi, Quận Hải An, Thành Phố Hải Phòng.\n– Cơ sở Bắc Giang: Đường Lý Tự Trọng, Phường Xương Giang, TP. Bắc Giang."
+    },
+    {
+        "Id": "qa5",
+        "Question": "Sau khi tốt nghiệp ra trường sinh viên sẽ được cấp bằng gì? Bằng của trường là do ai cấp?",
+        "Answer": "Sinh viên Phổ thông Cao đẳng sau 2 năm 4 tháng học ra trường sẽ được cấp bằng Cao đẳng Chính quy do Cao đẳng FPT Polytechnic cấp dưới sự quản lý của Bộ Lao động Thương Binh & Xã hội."
+    },
+    {
+        "Id": "qa6",
+        "Question": "Học xong trường có sắp xếp việc làm cho học sinh không?",
+        "Answer": "Phòng Quan hệ doanh nghiệp tại Phổ thông Cao đẳng sẽ hỗ trợ để giới thiệu việc làm cho các bạn sinh viên sau khi tốt nghiệp. Không chỉ sau khi học xong mà ngay trong quá trình học, các bạn sẽ được giới thiệu những công việc phù hợp với năng lực của mình. Hiện nay trường đang liên kết với hơn 500 doanh nghiệp trong và ngoài tập đoàn, cộng với những kĩ năng và kinh nghiệm sau 04 năm học các bạn sinh viên có thể hoàn toàn tự tin và không cần phải lo lắng về vấn đề việc làm sau khi ra trường."
+    },
+    {
+        "Id": "qa7",
+        "Question": "Sinh viên tốt nghiệp Phổ thông Cao đẳng FPT có thể học liên thông lên các trường đại học được hay không?",
+        "Answer": "Sinh viên sau tốt nghiệp có thể học liên thông lên các trường Đại học. Tùy thuộc vào trường Đại học sinh viên mong muốn theo học có ngành học liên thông tương đương với ngành học theo học tại Phổ thông Cao đẳng hay không. Sau đó người học phải trải qua kỳ thi liên thông của trường Đại học và xét duyệt các môn học còn thiếu để học bổ sung."
     }
     
 ]
