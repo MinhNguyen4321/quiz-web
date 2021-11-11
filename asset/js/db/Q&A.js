bfbcd67c151dabfea3c89cd0c8b1c553fa518298
Minh Nguyen <hoaiminh4321@gmail.com>
2021-11-11 07:20:11 +0700

    update

---
+++ asset/js/db/Q&A.js
@@ -0,0 +1,23 @@
+[
+    {
+        "Id": "qa1",
+        "Question": "Phổ thông Cao đẳng FPT được thành lập khi nào?",
+        "Answer": "Chương trình Phổ thông Cao đẳng tại Cao đẳng FPT Polytechnic được thành lập năm 2019, trực thuộc Tổ chức Giáo dục FPT – Một trong những Tổ chức Giáo dục uy tín, danh giá, có nhiều thành tựu, giải thưởng Quốc tế bậc nhất Việt Nam. "
+    },
+    {
+        "Id": "qa2",
+        "Question": "Điều kiện xét tuyển của Phổ thông Cao đẳng FPT là gì?",
+        "Answer": "Để được xét tuyển đầu vào tại Phổ thông Cao đẳng thí sinh cần có bằng chứng nhận tốt nghiệp THCS"
+    },
+    {
+        "Id": "qa3",
+        "Question": "Thủ tục nộp hồ sơ cần những giấy tờ gì?",
+        "Answer": "Hồ sơ xét tuyển bao gồm:\n- 01 Phiếu đăng ký học có dán ảnh và có dấu xác nhận của công an địa phương hoặc trường cấp 2 thí sinh đang theo học\n- 01 Bản sao công chứng giấy khai sinh hoặc 01 bản sao chứng minh thư nhân dân của thí sinh\n- 01 Bản sao công chứng bằng tốt nghiệp THCS hoặc Giấy chứng nhận tốt nghiệp tạm thời. (Chú ý: Thí sinh nộp giấy chứng nhận tốt nghiệp tạm thời phải bổ sung bản sao công chứng Bằng tốt nghiệp THCS ngay sau khi nhận được)\n- 02 ảnh 4×6"
+    },
+    {
+        "Id": "qa4",
+        "Question": "Trường có mấy cơ sở?",
+        "Answer": "Hiện tại Phổ thông Cao đẳng đang triển khai tại 09 cơ sở trên toàn quốc. Các cơ sở đào tạo tọa lạc tại các thành phố lớn, trung tâm của Việt Nam như: Hà Nội, Đà Nẵng, HCM, Tây Nguyên và Cần Thơ,…\n– Cơ sở Hà Nội: Đường Trịnh Văn Bô, phường Phương Canh, quận Nam Từ Liêm, Hà Nội.\n– Cơ sở Đà Nẵng: Số 137 Nguyễn Thị Thập, phường Hoà Minh, quận Liên Chiểu, TP. Đà Nẵng.\n- Cơ sở Tây Nguyên: Số 27, đường Nguyễn Tất Thành, Tân Lợi, TP. Buôn Mê Thuột, Đắk Lắk.\n- Cơ sở Hồ Chí Minh: Toà nhà Innovation, lô 24, Công viên phần mềm Quang Trung, Quận 12, Hồ Chí Minh.\n- Cơ sở Cần Thơ: Số 288, Nguyễn Văn Linh, phường An Khánh, quận Ninh Kiều, TP. Cần Thơ.\n– Cơ sở Đồng Nai: Số 120/68/39 Lê Thoa, Phường Tân Tiến, TP. Biên Hòa, Đồng Nai.\n– Cơ sở Huế:  Số 16 Lâm Hoằng, Phường Vỹ Dạ, TP. Huế.\n– Cơ sở Hải Phòng:  118A Cát Bi, Quận Hải An, Thành Phố Hải Phòng.\n– Cơ sở Bắc Giang: Đường Lý Tự Trọng, Phường Xương Giang, TP. Bắc Giang."
+    }
+    
+]
